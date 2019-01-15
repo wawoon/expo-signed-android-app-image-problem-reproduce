f87874ca60e4dd2c16ed04dc1e7a83cf0d596a2f
Yoshinori Kosaka <yoshinori.kosaka@welyco.com>
2019-01-16 06:44:54 +0900

    initial commit

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "exp0faa4c1a8a8442398afc5b1b7e3dc3c3://192.168.11.6:80";
+
+}
